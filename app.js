0aea8b5c0466fbff6463756b68b85ab588c5625f
MattiaToscano <mattia1997ct@gmail.com>
2025-05-29 13:26:22 +0200

    [UPD] Preso esercizio di ieri, procedo al riadattamento per la consegna odierna.

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use('/blog', blogRouter);
 
 
 
-//Metto il server in ascolto sulla porta 3000
+//Metto il server in ascolto sulla porta 300
 app.listen(port, () => {
     console.log(`Il server è in ascolto sulla porta http://localhost:${port}`);
 })
